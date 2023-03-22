a102c1d38a03a0e54e57e4896e371bf99380c395
Eduardo Villagra <cubalosadaperla@gmail.com>
2023-03-22 15:35:33 -0300

    codigo actualizado

--- db.sql
+++ db.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE IF NOT EXISTS test;
 Use test;
+
+CREATE TABLE cursos 
+    id INT(11) NOT NULL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    profesor Text TO NULL, 
+    price Decimal(14, 3) NOT NULL
+
+    
