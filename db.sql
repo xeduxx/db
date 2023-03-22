60c2e3de7647f087c21ea67d4bd93f230dc9cf2f
Eduardo Villagra <cubalosadaperla@gmail.com>
2023-03-22 16:09:09 -0300

    codigo actualizado

--- db.sql
+++ db.sql
@@ -7,5 +7,20 @@ CREATE TABLE cursos
     description TEXT NOT NULL,
     profesor Text TO NULL, 
     price Decimal(14, 3) NOT NULL
+);
 
-    
+CREAR  TABLA  SI NO EXISTE profesores(
+	id INT ( 11 ) NO NULO  CLAVE PRIMARIA AUTO_INCREMENTO,
+	nombre TEXTO  NO NULO ,
+	descripción TEXTO  NO NULO ,
+	fecha_insertar FECHA  NO NULA POR DEFECTO AHORA()
+);
+
+CREAR  TABLA  SI NO EXISTE transacciones (
+	id INT ( 11 ) NO NULO  CLAVE PRIMARIA AUTO_INCREMENTO,
+	date_insert FECHA  NO NULA POR DEFECTO AHORA(),
+	cantidad DECIMAL ( 14 , 3 ) NO NULO ,
+	curso TEXTO  NO NULO ,
+	profesor TEXTO  NO NULO ,
+	impuesto DECIMAL ( 14 , 3 ) NO NULO
+); 
